4027799aba273f54ef7d2f76c7705c3538927bb3
HamzaAli303 <alihamza.m1999@gmail.com>
2023-04-19 14:55:55 -0600

    Movie Page

---
+++ source.js
@@ -0,0 +1,46 @@
+console.log('movie source')
+
+let message = document.querySelector('#message');
+
+
+let addMovie = (event) => {
+    event.preventDefault()
+    let inputField = document.querySelector('input');
+    let movie = document.createElement('li');
+    let movieTitle = document.createElement('span');
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener('click', crossOffMovie);
+    movie.appendChild(movieTitle);
+    document.querySelector('ul').appendChild(movie);
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'x';
+    deleteBtn.addEventListener('click', deleteMovie);
+    movie.appendChild(deleteBtn);
+
+}
+
+document.querySelector('form').addEventListener('submit', addMovie);
+
+
+let deleteMovie = (event) => {
+    event.target.parentNode.remove();
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`;
+    revealMessage();
+}
+
+
+let crossOffMovie = (event) => {
+    event.target.classList.toggle('checked');
+    if (event.target.classList.contains('checked')) {
+        message.textContent = `${event.target.textContent} Watched!`
+    } else {
+        message.textContent = `${event.target.textContent} added back!`
+    }
+    revealMessage();
+}
+
+let revealMessage = () => {
+    message.classList.remove('hide');
+    setTimeout(() => (message.classList.add('hide')), 1000);
+}
+
